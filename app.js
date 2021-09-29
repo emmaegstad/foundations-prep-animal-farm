cacc4966d59903ef1094e6525be4eb207a97a411
Emma Egstad <emma@egstad.com>
2021-09-29 15:52:57 -0700

    updated grid

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-// import functions and grab DOM elements
+const catImage = document.getElementById('cat-image');
+const dogImage = document.getElementById('dog-image');
+const horseImage = document.getElementById('horse-image');
+const meow = new Audio('assets/meow.wav');
+const bark = new Audio('assets/bark.wav');
+const neigh = new Audio('assets/neigh.wav');
 
-// initialize global state
+catImage.addEventListener('click', () => {
+    meow.currentTime = 0;
+    meow.play();
+    catImage.classList.add('selected');
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+dogImage.addEventListener('click', () => {
+    bark.currentTime = 0;
+    bark.play();
+    dogImage.classList.add('selected');
+});
+
+horseImage.addEventListener('click', () => {
+    neigh.currentTime = 0;
+    neigh.play();
+    horseImage.classList.add('selected');
+});
+
+document.addEventListener('keyup', event => {
+    if (event.key === 'c') {
+        meow.currentTime = 0;
+        meow.play();
+        catImage.classList.add('selected');
+    } else if
+        (event.key === 'd') {
+        bark.currentTime = 0;
+        bark.play();
+        dogImage.classList.add('selected');
+    } else if
+        (event.key === 'h') {
+        neigh.currentTime = 0;
+        neigh.play();
+        horseImage.classList.add('selected');
+    }
+}
+);
+
+const onAnimationEnd = (ev) => {
+    ev.target.classList.remove('selected');
+};
+
+catImage.addEventListener('animationend', onAnimationEnd);
+dogImage.addEventListener('animationend', onAnimationEnd);
+horseImage.addEventListener('animationend', onAnimationEnd);
